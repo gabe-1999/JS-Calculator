0097df8871ba4c7526a2e3b19d35a58313688fb6
gabe-1999 <jensgabrieltrabert56@gmail.com>
2023-03-05 05:11:33 -0600

    Equal button functionality

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const keys = document.querySelectorAll(".key");
+const screen  = document.getElementById("screen");
 const scrText = document.getElementById("scr-text");
 const values = {}
+let isLocked = false;
+let operation = "";
+let result = "";
+
 
 function addValueMappings() {
     keys.forEach((key) => {
@@ -10,13 +15,47 @@ function addValueMappings() {
 }
 
 addValueMappings();
-console.log(values);
+
 
 keys.forEach((key) => {
     key.addEventListener("click" , handleClick);
 });
 
 function handleClick() {
-    scrText.innerHTML += values[this.id];
-    if (this.id == "key-13") scrText.innerHTML = "";
+    let result;
+    if (this.id == "key-15") {
+        result = calculate(operation);
+        console.log(result);
+        showDisplay(result, (parseInt(result)) ? false : true);
+    }else if(this.id == "key-13") {
+        clear();
+    } else {
+        scrText.innerHTML += values[this.id];
+        operation += values[this.id];
+        console.log(scrText.offsetWidth, screen.offsetWidth);
+    }
+    
+}
+
+function calculate(op) {
+    try {
+        return eval(operation); 
+    } catch (error) {
+        return "Invalid Expression!"; 
+    }
+}
+
+function showDisplay(value, isError = false) {
+    if (isError) {
+        scrText.classList.add("error");
+        scrText.innerHTML = value;
+    } else {
+        scrText.innerHTML = value;
+    }
+}
+
+function clear() {
+    scrText.innerHTML = "";
+    scrText.classList.remove("error");
+    operation = "";
 }
