c5b7fc6caf127699e180f4c6e95ce70653b50f5b
gabe-1999 <jensgabrieltrabert56@gmail.com>
2023-03-10 02:36:33 -0600

    Contents in screen do not go out of bouds fix

--- app.js
+++ app.js
@@ -1,47 +1,44 @@
-const keys = document.querySelectorAll(".key");
+const numKeys = document.querySelectorAll(".key");
 const screen  = document.querySelector(".screen");
 const text = screen.querySelector('.scr-text');
-const values = {}
-let isLocked = false;
+const maxChar = 16;
+const valueMap = {}
+const errorMessage = "Invalid Expression!";
 let operation = "";
 let result = "";
 
 
 function addValueMappings() {
-    keys.forEach((key) => {
+    numKeys.forEach((key) => {
         let value = key.querySelector("p").innerHTML;
-        values[key.id] = value;
+        valueMap[key.id] = (value == "X") ? "*" : value;
     })
 }
 
 addValueMappings();
 
 
-keys.forEach((key) => {
+numKeys.forEach((key) => {
     key.addEventListener("click" , handleClick);
 });
 
 function handleClick() {
-    let result;
     if (this.id == "key-15") {
-        result = calculate(operation);
-        console.log(result);
-        showDisplay(result, (parseInt(result)) ? false : true);
+        calculate();
     }else if(this.id == "key-13") {
         clear();
     } else {
-        text.innerHTML += values[this.id];
-        operation += values[this.id];
-        console.log(screen.offsetWidth, screen.offsetWidth);
+        showDisplay(this.id);
     }
     
 }
 
-function calculate(op) {
+function calculate() {
     try {
-        return eval(operation); 
+        result = eval(operation).toString();
+        showResult();
     } catch (error) {
-        return "Invalid Expression!"; 
+        showDisplay(operation, (parseInt(operation)) ? false : true);
     }
 }
 
@@ -50,8 +47,19 @@ function showDisplay(value, isError = false) {
         text.innerHTML = value;
         text.style.color = "crimson";
     } else {
-        text.innerHTML = value;
+        operation += valueMap[value];
+        let content = operation.length > maxChar ? trimString(operation) : operation
+        text.innerHTML = (text.innerHTML == errorMessage) ? errorMessage : content;
     }
+}
+
+function trimString(str) {
+    return str.substring(str.length - 15, str.length) + "..";
+}
+
+
+function showResult() {
+    text.innerHTML = result.length > maxChar  ? trimString(result) : result;
 }
 
 function clear() {
